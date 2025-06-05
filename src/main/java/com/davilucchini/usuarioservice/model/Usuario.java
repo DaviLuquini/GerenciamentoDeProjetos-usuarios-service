f08d40aad2ec529441cc363e214b1bcb2b4f9af2
Davi Luquini <davi262016@gmail.com>
2025-06-05 20:34:48 -0300

    update

--- src/main/java/com/davilucchini/usuarioservice/model/Usuario.java
+++ src/main/java/com/davilucchini/usuarioservice/model/Usuario.java
@@ -1,6 +1,10 @@
 package com.davilucchini.usuarioservice.model;
 
-import javax.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Usuario {
